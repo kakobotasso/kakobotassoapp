d95ea95580755cc700745bc7e90f907984548809
Kako Botasso <kakodev@gmail.com>
2016-09-25 13:30:56 -0300

    Commit inicial

---
+++ app/src/main/java/br/com/ezeqlabs/kakobotasso/EzeqlabsActivity.java
@@ -0,0 +1,13 @@
+package br.com.ezeqlabs.kakobotasso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EzeqlabsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ezeqlabs);
+    }
+}
